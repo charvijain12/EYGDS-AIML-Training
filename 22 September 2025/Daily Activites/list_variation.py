991e6d63e5be342796f729e1ab3c6d8c2d3354c6
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2025-09-22 12:20:37 +0530

    Add files via upload

---
+++ 22 September 2025/Daily Activites/list_variation.py
@@ -0,0 +1,7 @@
+colors = ['black','white','blue']
+
+colors.append('red') #add at end
+colors.insert(1,'green') #insert at index 1
+colors.remove('red') #remove by value
+colors.pop() #remove last element
+print(colors)
