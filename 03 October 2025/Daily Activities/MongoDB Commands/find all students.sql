d35bb61287e7ca3120a125e3e967ea0ae5abaa7a
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2025-10-03 17:00:51 +0530

    Day 8

---
+++ 03 October 2025/Daily Activities/MongoDB Commands/find all students.sql
@@ -0,0 +1,40 @@
+//find all students
+db.students.find()
+
+//output
+{
+  _id: ObjectId('68dfa867d820ec06268084b3'),
+  student_id: 2,
+  name: 'Priya',
+  age: 22,
+  city: 'Delhi',
+  course: 'ML',
+  marks: 90
+}
+{
+  _id: ObjectId('68dfa867d820ec06268084b4'),
+  student_id: 3,
+  name: 'Arjun',
+  age: 20,
+  city: 'Bengaluru',
+  course: 'Data Science',
+  marks: 78
+}
+{
+  _id: ObjectId('68dfa867d820ec06268084b5'),
+  student_id: 4,
+  name: 'Neha',
+  age: 23,
+  city: 'Hyderabad',
+  course: 'AI',
+  marks: 88
+}
+{
+  _id: ObjectId('68dfa867d820ec06268084b6'),
+  student_id: 5,
+  name: 'Vikram',
+  age: 21,
+  city: 'Chennai',
+  course: 'ML',
+  marks: 95
+}
