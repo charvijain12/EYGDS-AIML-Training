7c2ba8f364ecb09bf96539b4e15adb7aba721fe1
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2025-10-06 09:38:05 +0530

    Day 8

---
+++ 03 October 2025/Tasks/MongoDB commands/find all.sql
@@ -0,0 +1,36 @@
+//find all
+db.teachers.find()
+
+//output
+{
+  _id: ObjectId('68e33dcc3a63827502152c57'),
+  id: 1,
+  name: 'Dr. P Mehta',
+  subject: 'Physics',
+  experience: 12,
+  city: 'Delhi'
+}
+{
+  _id: ObjectId('68e33dcc3a63827502152c58'),
+  id: 2,
+  name: 'Prof. K Iyer',
+  subject: 'Mathematics',
+  experience: 8,
+  city: 'Chennai'
+}
+{
+  _id: ObjectId('68e33dcc3a63827502152c59'),
+  id: 3,
+  name: 'Mrs. N Kapoor',
+  subject: 'Computer Science',
+  experience: 10,
+  city: 'Hyderabad'
+}
+{
+  _id: ObjectId('68e33dcc3a63827502152c5a'),
+  id: 4,
+  name: 'Mr. SP Singh',
+  subject: 'Chemistry',
+  experience: 6,
+  city: 'Bangalore'
+}
