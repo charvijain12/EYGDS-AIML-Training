c7d430a691c8d60884771a43a528e32c7c1a9d39
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2025-10-06 10:03:22 +0530

    Day 8

---
+++ 03 October 2025/Tasks/MongoDB commands/find by field.sql
@@ -0,0 +1,12 @@
+//find by field
+db.teachers.find({subject:"Physics"});
+
+//output
+{
+  _id: ObjectId('68e33dcc3a63827502152c57'),
+  id: 1,
+  name: 'Dr. P Mehta',
+  subject: 'Physics',
+  experience: 12,
+  city: 'Delhi'
+}
