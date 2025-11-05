07990b786e5b6359c19062646e9525a5aea3aff7
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2025-11-05 10:51:47 +0530

    Day 28

--- 03 November 2025/agent.py
+++ 03 November 2025/agent.py
@@ -1,37 +1,13 @@
-# Import the dependencies
-from autogen_agentchat.agents import AssistantAgent
-from autogen_ext.models.openai import OpenAIChatCompletionClient
-from dotenv import load_dotenv
-import os
-import asyncio
+from autogen import AssistantAgent, UserProxyAgent
 
-# Load the environment variables
-# You should always use a .env file to store your API keys
-load_dotenv()
-
-# Create the open router model client
-open_router_model_client = OpenAIChatCompletionClient(
-    base_url="https://openrouter.ai/api/v1",
-    model="mistralai/mistral-7b-instruct:free",
-    api_key="add your key",
-    model_info={
-        "family": "openai",
-        "vision": True,
-        "function_calling": True,
-        "json_output": False
-    }
-)
-
-# Create an Agent using the open router model client created in previous step
 assistant = AssistantAgent(
-    name="myassistent",
-    model_client=open_router_model_client,
-    system_message="You are a helpful assistant that answers questions accurately and concisely."
+    name="coder",
+    system_message="You are a helpful Python coder."
 )
 
-# Ask any question to your agent and see the result
-async def main() -> None:
-    result = await assistant.run(task="What is the capital of India?")
-    print(f"Result: {result.messages[-1].content}")
+user = UserProxyAgent(
+    name="charvi",
+    human_input_mode="NEVER"
+)
 
-asyncio.run(main())
+user.initiate_chat(assistant, message="Write a Python function to calculate Fibonacci numbers.")
