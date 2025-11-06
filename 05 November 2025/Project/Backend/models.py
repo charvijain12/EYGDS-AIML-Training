a80489e9eebba692c7f7b03d07dcb6acf3abeebe
Charvi Jain <97164074+charvijain12@users.noreply.github.com>
2025-11-06 10:54:22 +0530

    Day 29

--- 05 November 2025/Project/Backend/models.py
+++ 05 November 2025/Project/Backend/models.py
@@ -1,18 +1,23 @@
-from sqlalchemy import Column, Integer, String, JSON
+# models.py
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import JSON
 from database import Base
 
 class Employee(Base):
     __tablename__ = "employees"
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    email = Column(String, unique=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
     skills = Column(JSON)
     past_projects = Column(JSON, default=[])
 
 class Project(Base):
     __tablename__ = "projects"
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     description = Column(String)
     required_skills = Column(JSON)
-    status = Column(String)
+    status = Column(String, default="open")
